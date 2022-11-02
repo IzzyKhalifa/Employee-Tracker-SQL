7c2a3ce8921f8c87dd0808e3a636f16a8d9250a6
IzzyKhalifa <izzy.e.khalifa@gmail.com>
2022-11-02 15:24:18 +1100

    Added init function and sql files

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
 const { connection } = require("./db/connection");
 const inquirer = require("inquirer");
+
+function employeeView() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View All Departments",
+          "View All Roles",
+          "View All Employees",
+          "Add Department",
+          "Add Role",
+          "Add Employee",
+          "Update Employee Role",
+          "Quit",
+        ],
+      })
+      .then((res) => {
+        let choice = res.action;
+        switch (choice) {
+          case "View All Departments":
+            departmentsView();
+            break;
+          case "View All Roles":
+            rolesView();
+            break;
+          case "View All Employees":
+            employeesView();
+            break;
+          case "Add Department":
+            addDepartment();
+            break;
+          case "Add Role":
+            addRole();
+            break;
+          case "Add Employee":
+            addEmployee();
+            break;
+          case "Update Employee Role":
+            updateEmployeeRole();
+            break;
+          case "Quit":
+            connection.end();
+            break;
+        }
+      });
